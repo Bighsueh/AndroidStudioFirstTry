e865fd942153316fc68b4829cd9c5f9f1d35d04c
Hsueh <40841121@gm.nfu.edu.tw>
2020-08-11 15:27:58 +0800

    更改"出拳選擇"為AlertDialog

--- app/src/main/java/com/example/androidstudiofirsttry/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiofirsttry/MainActivity.kt
@@ -1,36 +1,48 @@
 package com.example.androidstudiofirsttry
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val array = arrayOf("剪刀", "石頭", "布")
+    var position =0
 
+    //使用預設Toast顯示文字訊息
+    private fun showToast(text: String) =
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_scissor.isChecked = true
+        btn_action.setOnClickListener {
+            //宣告變數用以保存選擇位置
+            //宣告AlertDialog物件，setSingleChoiceItems可以在Dialog顯示單選框
+            AlertDialog.Builder(this)
+                .setTitle("出拳") //顯示標題
+                .setSingleChoiceItems(array, 0) { dialogInterface, i ->
+                    position = i
+                }
+                .setPositiveButton("確定") { dialog, which ->
+                    showToast("你出的是" + array[position])
+                }.show()    //顯示Dialog
+        }
+
         btn_mora.setOnClickListener {
+
             if(!user_name.text.equals("")){
                 text_name.text = "名字\n${user_name.text}"
             }
-            //我方出拳
-            var user_punch = 1
-            if (btn_scissor.isChecked) {
-                text_mmora.text = "我方出拳\n剪刀"
-                user_punch = 1
-            }
-            else if(btn_stone.isChecked){
-                text_mmora.text = "我方出拳\n石頭"
-                user_punch = 2
-            }
-            else if(btn_paper.isChecked){
-                text_mmora.text = "我方出拳\n布"
-                user_punch = 3
-            }
+
+            text_mmora.text = "我方出拳\n${array[position]}"
+
         //電腦出拳
-        var r = (Math.random()*3+1).toInt()
-        when(r){
+        var con = (Math.random()*3+1).toInt()
+        when(con){
             1->text_cmora.text="電腦出拳\n剪刀"
             2->text_cmora.text="電腦出拳\n石頭"
             3->text_cmora.text="電腦出拳\n布"
@@ -38,15 +50,15 @@ class MainActivity : AppCompatActivity() {
         //勝負判定
         when{
             //判定玩家獲勝
-            user_punch==1 && r==3 ||
-            user_punch==2 && r==1 ||
-            user_punch==3 && r==2->{
+            position==0 && con==3 ||
+            position==1 && con==1 ||
+            position==2 && con==2->{
                 text_winner.text = "勝利者\n${user_name.text}"
             }
             //判定電腦獲勝
-            user_punch==3 && r==1 ||
-            user_punch==1 && r==2 ||
-            user_punch==2 && r==3->{
+            position==2 && con==1 ||
+            position==0 && con==2 ||
+            position==1 && con==3->{
                 text_winner.text = "勝利者\n電腦"
             }
             else->{
